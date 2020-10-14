cd2dc423a51584b4c1bcaadd46c3f1cb28101354
Timendus <mail@timendus.com>
2020-10-14 11:11:46 +0200

    Add new images to fix the S-bends, and change hall-5 a bit

--- screens/combine.sh
+++ screens/combine.sh
@@ -25,15 +25,29 @@ cat wall-1.txt >> screens.txt
 # Outer variations
 sed -n -e 1,8p -e 25,33p wall-1-1.txt >> screens.txt
 sed -n -e 1,8p -e 25,33p wall-1-2.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-1-3.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-1-4.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-1-5.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-1-6.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-2-3.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-2-4.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-2-5.txt >> screens.txt
+sed -n -e 1,8p -e 25,33p wall-2-6.txt >> screens.txt
 
 # Inner variations
 sed -n 9,25p wall-2.txt >> screens.txt
 sed -n 9,25p wall-3.txt >> screens.txt
 sed -n 9,25p wall-3-3.txt >> screens.txt
+sed -n 9,25p wall-3-4.txt >> screens.txt
+sed -n 9,25p wall-3-5.txt >> screens.txt
+sed -n 9,25p wall-3-6.txt >> screens.txt
 sed -n 9,25p wall-4.txt >> screens.txt
 sed -n 9,25p wall-4-3.txt >> screens.txt
 sed -n 9,25p wall-4-4.txt >> screens.txt
+sed -n 9,25p wall-4-5.txt >> screens.txt
+sed -n 9,25p wall-4-6.txt >> screens.txt
 sed -n 9,25p wall-5.txt >> screens.txt
 sed -n 9,25p wall-5-3.txt >> screens.txt
 sed -n 9,25p wall-5-5-3.txt >> screens.txt
 sed -n 9,25p wall-5-5.txt >> screens.txt
+sed -n 9,25p wall-5-6.txt >> screens.txt
