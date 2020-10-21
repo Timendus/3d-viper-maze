674b1534ba0b1811422196bc1dbf5e70f9c86a19
Timendus <mail@timendus.com>
2020-10-21 22:12:18 +0200

    Swap out the exit "coin" for a tunnel (needs pre-XOR-ing)

--- screens/combine.sh
+++ screens/combine.sh
@@ -58,6 +58,7 @@ sed -n 9,25p wall-5-6-3.txt >> screens.txt
 sed -n 1,29p sprites.txt >> screens.txt
 sed -n 9,25p viper4.txt >> screens.txt
 sed -n 9,25p viper-neck.txt >> screens.txt
+sed -n 25,33p sprites2.txt >> screens.txt
 
 # Bitmaps
 cat map-victory.txt >> screens.txt
